1e6c2d1d4bf1e017f9b1b5c26177a1fb4fc73323
Cristian Gonzalez <crdgonzalez@unal.edu.co>
2016-11-07 14:42:39 -0500

    profile

--- DentistwareD/application/views/perfil_view.php
+++ DentistwareD/application/views/perfil_view.php
@@ -8,99 +8,111 @@
                 <div class="box box-primary">
                 	<div class="box-body">
                  		<div class="col-xs-12 text-center ">
-							<?php
-								$url = base_url() . '/assets/img/avatar5.png';
-                                echo '<img src="' . $url . '" class="user-image" alt="User Image">';
+							<?php							
+								
+								
+								$tipo = $persona->tipo_persona;
+								$route = '';
+								switch ($tipo) {
+									case "ADM":
+										$route = 'admin';
+										break;
+									case "CLT":
+										$route = 'cliente';
+										break;
+									case "ODO":
+										$route = 'odonto';
+										break;
+									case "EMP":
+										$route = 'empleado';
+										break;
+								}
+								if($persona->foto_persona){
+									echo '<img id="foto_img" class="center-block" height="200" width="200" src="'.base_url() . "uploads/" . $route . '/'  . $persona->foto_persona . '">';										
+								} else {
+									echo '<img id="foto_img" class="center-block" height="200" width="200" src="' . base_url("assets/img/foto-default.png") . '">';
+								}
+								
                                 echo heading('Información Personal', 3);
 							?>
 						</div>                 
                             <div class="text-align:center;">
                                <table style="width:60" align="center">
-                                   <?php
-
-                                  echo "<tr>";                                   
-                                    echo "<th class= text-right style= width:45% >Nombre(s) y Apellidos:</th>";
-                                      echo "<td style= width:10% > </td>";
-                                      echo "<td style= width:45% > " . ucwords($persona->nombre_persona) . "</td>";
-                                  echo "</tr>";
+                             	<?php
+                                   echo "<tr>";
+                                   echo "<th class= text-right style= width:45% >Nombre(s) y Apellidos:</th>";
+                                   echo "<td style= width:10% > </td>";
+                                   echo "<td style= width:45% > " . ucwords($persona->nombre_persona) . "</td>";
+                                   echo "</tr>";
 
                                   echo "<tr>";
-                                    echo "<th class= text-right style= width:45% >Documento:</td>";
-                                      echo "<td style= width:10% > </td>";
-                                    echo "<td style= width:45% > " . $persona->tipo_documento . ' &nbsp &nbsp' . $persona->documento_persona . " </td>";
+                                  echo "<th class= text-right style= width:45% >Documento:</td>";
+                                  echo "<td style= width:10% > </td>";
+                                  echo "<td style= width:45% > " . $persona->tipo_documento . '. &nbsp' . $persona->documento_persona . " </td>";
                                   echo "</tr>";
-
-                                   echo "<tr>";
-                                    echo "<th class= text-right style= width:45% >Edad:</td>";
-                                      echo "<td style= width:10% > </td>";
-                                    echo "<td style= width:45% > " . $persona->edad_persona .' '. 'Años' . "</td>";
-                                  echo "</tr>";
-
+                                  
                                   echo "<tr>";
-                                    echo "<th class= text-right style= width:45% >Fecha de Nacimiento:</th>";
-                                      echo "<td style= width:10% > </td>";
-                                    echo "<td style= width:45% > " . $persona->fecha_nacimiento . "</td>";
+                                  echo "<th class= text-right style= width:45% >Telefono:</th>";
+                                  echo "<td style= width:10% > </td>";
+                                  echo "<td style= width:45% > " . $persona->telefono_persona . "</td>";
                                   echo "</tr>";
-
-                                echo "<tr>";
-                                    echo "<th class= text-right style= width:45% >Genero:</th>";
-                                      echo "<td style= width:10% > </td>";
-                                    if ($persona->genero_persona == 'M'){
-                                          echo "<td style= width:45% > Masculino</td>";
-                                        }else{
-                                            echo "<td style= width:45% > Femenino</td>";
-                                        };
+                                  
+                                  echo "<tr>";
+                                  echo "<th class= text-right style= width:45% >Ubicación:</th>";
+                                  echo "<td style= width:10% > </td>";
+                                  echo "<td style= width:45% > " . ucwords(strtolower($persona->direccion_persona)) . "</td>";
                                   echo "</tr>";
-                                   if($persona->tipo_persona != 'ADM'){
-                                       echo "<tr>";
-                                        echo "<th class= text-right style= width:45% >Tipo de Sandre y RH:</th>";
-                                          echo "<td style= width:10% > </td>";
-                                        echo "<td style= width:45% > " . $persona->tipo_sangre_cliente . ' ' . $persona->rh_cliente . "</td>";
-                                        echo "</tr>";
-                                   };
-                                   echo "<tr>";
-                                    echo "<th class= text-right style= width:45% >Estado:</th>";
-                                      echo "<td style= width:10% > </td>";
-                                     if ($persona->estado_persona == 'ACT'){
-                                         echo "<td style= width:45% > Activo</td>";
-                                        }else{
-                                         echo "<td style= width:45% > Retirado</td>";
-                                        };
+                                  
+                                  echo "<tr>";
+                                  echo "<th class= text-right style= width:45% >Ciudad:</th>";
+                                  echo "<td style= width:10% > </td>";
+                                  echo "<td style= width:45% > " . ucfirst(mb_strtolower($persona->nombre_ciudad, 'UTF-8')) . " - " . ucfirst(mb_strtolower($persona->nombre_dept, 'UTF-8')) . "</td>";
                                   echo "</tr>";
+                                  
+                                  echo "<tr>";
+                                  echo "<th class= text-right style= width:45% >Fecha de Nacimiento:</th>";
+                                  echo "<td style= width:10% > </td>";
+                                  echo "<td style= width:45% > " . $persona->fecha_nacimiento . "</td>";
+                                  echo "</tr>";
+                                  
+                                  echo "<tr>";
+                                  echo "<th class= text-right style= width:45% >Edad:</td>";
+                                  echo "<td style= width:10% > </td>";
+                                  echo "<td style= width:45% > " . $persona->edad_persona .' '. 'Años' . "</td>";
+                                  echo "</tr>";
+                                  
+                                  echo "<tr>";
+                                  echo "<th class= text-right style= width:45% >Genero:</th>";
+                                  echo "<td style= width:10% > </td>";
+                                  
+                                  if ($persona->genero_persona == 'M'){
+                                  		echo "<td style= width:45% > Masculino</td>";
+								  }else{
+                                  		echo "<td style= width:45% > Femenino</td>";
+                                  }
+                                  echo "</tr>";
+                                   
+                                   if($persona->tipo_persona != 'ADM' && $persona->tipo_persona != 'EMP'){
+	                                   	echo "<tr>";
+	                                   	echo "<th class= text-right style= width:45% >Tipo de Sangre y RH:</th>";
+	                                   	echo "<td style= width:10% > </td>";
+	                                   	echo "<td style= width:45% > " . $persona->tipo_sangre_cliente . ' ' . $persona->rh_cliente . "</td>";
+	                                   	echo "</tr>";
+                                   }
 
                                    if($persona->tipo_persona == 'CLT'){
+	                                   	echo "<tr>";
+	                                   	echo "<th class= text-right style= width:45% >Eps:</th>";
+	                                   	echo "<td style= width:10% > </td>";
+	                                   	echo "<td style= width:45% > " . $persona->eps_persona . "</td>";
+	                                   	echo "</tr>";
+                                   }
+                                   
                                    echo "<tr>";
-                                    echo "<th class= text-right style= width:45% >Eps de Usuario:</th>";
-                                      echo "<td style= width:10% > </td>";
-                                    echo "<td style= width:45% > " . $persona->eps_persona . "</td>";
-                                  echo "</tr>";
-                                   };
-
-                                   echo "<tr>";
-                                    echo "<th class= text-right style= width:45% >Ciudad:</th>";
-                                      echo "<td style= width:10% > </td>";
-                                    echo "<td style= width:45% > " . ucfirst(mb_strtolower($persona->nombre_ciudad, 'UTF-8')) . "</td>";
-                                  echo "</tr>";
-
-                                   echo "<tr>";
-                                    echo "<th class= text-right style= width:45% >Direccion:</th>";
-                                      echo "<td style= width:10% > </td>";
-                                    echo "<td style= width:45% > " . ucwords(strtolower($persona->direccion_persona)) . "</td>";
-                                  echo "</tr>";
-
-                                   if($persona->tipo_persona != 'ADM'){
-                                       echo "<tr>";
-                                        echo "<th class= text-right style= width:45% >E-mail:</th>";
-                                          echo "<td style= width:10% > </td>";
-                                        echo "<td> " . strtolower($persona->correo_persona) . "</td>";
-                                      echo "</tr>";
-                                   };
-                                       
-                                   echo "<tr>";
-                                    echo "<th class= text-right style= width:45% >Telefono:</th>";
-                                      echo "<td style= width:10% > </td>";
-                                    echo "<td style= width:45% > " . $persona->telefono_persona . "</td>";
-                                  echo "</tr>";
+                                   echo "<th class= text-right style= width:45% >E-mail:</th>";
+                                   echo "<td style= width:10% > </td>";
+                                   echo "<td> " . strtolower($persona->correo_persona) . "</td>";
+                                   echo "</tr>";                                      
                                    
                                    
                                    if($persona->tipo_persona == 'CLT'){
@@ -115,7 +127,7 @@
                                           echo "<td style= width:10% > </td>";
                                         echo "<td style= width:45% > " . $persona->telefono_contacto_cliente . "</td>";
                                       echo "</tr>";
-                                    };
+                                    }
                                    
                                    if($persona->tipo_persona == 'ODO'){
                                        echo "<tr>";
@@ -123,20 +135,14 @@
                                               echo "<td style= width:10% > </td>";
                                             echo "<td style= width:45% > " . $persona->estudios_odont . "</td>";
                                           echo "</tr>";
-                                    };
+                                    }
                                    ?>
                                 </table>
                             </div>
-                            <div class="col-xs-12 text-center">
-                                <BR></BR>
-                                <BR></BR>
-                                
-                                     <a href="<?php echo base_url("Perfil/edit_view/")?>"> <i type="button"  class="btn btn-info">Modificar Perfil</i> </a>
-                                
-                            </div>        
-                                            
-                                            
-                                            
+                            <div class="col-xs-12 text-center"> 
+                            	<br>                               
+                            	<a href="<?php echo base_url("Perfil/edit_view/")?>" class="btn btn-primary"> Modificar Perfil</a>                                
+                            </div>                                                                                                                                          
                     </div>                                     
                 </div>						   
             </div>
