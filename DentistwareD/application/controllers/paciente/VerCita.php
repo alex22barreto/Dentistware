a381f65d8047e732660fccbcdb89e6b9f949de64
Cristian Gonzalez <crdgonzalez@unal.edu.co>
2016-11-07 12:08:47 -0500

    view appointments

--- DentistwareD/application/controllers/paciente/VerCita.php
+++ DentistwareD/application/controllers/paciente/VerCita.php
@@ -6,12 +6,12 @@ class VerCita extends Cliente_Controller{
     public function __construct(){
         parent::__construct();
         $this->data ['page_title_end'] = '| Multas';
-        $this->load->model('multa_model');
+        $this->load->model('cita_model');
+        $this->data['citas'] = $this->cita_model->get_citas();
+        $this->get_user_menu('main-citas','citas-agendadas');
     }
     
     public function index(){
-        $this->get_user_menu('main-multas');
-        $this->data['multas'] = $this->multa_model->get_multas_cliente($this->session->userdata('id_persona'));
         $this->render('cliente/ver_cita_view');
     }
 
